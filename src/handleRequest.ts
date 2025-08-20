ad6b66dea398920134650a585af329ada22ff9c0
喵哩个咪 <wailovet@163.com>
2025-08-20 17:05:18 +0800

    Update handleRequest.ts

--- src/handleRequest.ts
+++ src/handleRequest.ts
@@ -32,9 +32,9 @@ export async function handleRequest(context: Context) {
   // otherwise it is definitely not a request to gemini,
   // and it must contain a token,
   // otherwise it definitely does not have permission.
-  if (!searchParams.has('key') || !(pathname.startsWith('/api/v1') || pathname.startsWith('/api/v1beta'))) {
-    return createErrorResponse('No permission', 401)
-  }
+  // if (!searchParams.has('key') || !(pathname.startsWith('/api/v1') || pathname.startsWith('/api/v1beta'))) {
+  //   return createErrorResponse('No permission', 401)
+  // }
 
   // The content of the request cannot be empty.
   const { headers: reqHeaders, body } = request
